6bc26d59bd88dd6d09a7831864995fc927957f8d
crashmars <crashmars@gmail.com>
2018-11-23 23:55:54 -0300

    Tienda 1.1

--- includes/functions.php
+++ includes/functions.php
@@ -19,6 +19,7 @@ if (!defined('IN_PHPBB'))
 	exit;
 }
 require_once('functions_ficha.php');
+require_once('functions_shop.php');
 // Common global functions
 /**
 * Load the autoloaders added by the extensions.
@@ -4588,6 +4589,7 @@ function page_header($page_title = '', $display_online_list = false, $item_id = 
 		'L_INDEX'			=> ($config['board_index_text'] !== '') ? $config['board_index_text'] : $user->lang['FORUM_INDEX'],
 		'L_SITE_HOME'		=> ($config['site_home_text'] !== '') ? $config['site_home_text'] : $user->lang['HOME'],
 		'L_ONLINE_EXPLAIN'	=> $l_online_time,
+		'L_SHOP_LIST'		=> 'Tienda',
 
 		'U_PRIVATEMSGS'			=> append_sid("{$phpbb_root_path}ucp.$phpEx", 'i=pm&amp;folder=inbox'),
 		'U_RETURN_INBOX'		=> append_sid("{$phpbb_root_path}ucp.$phpEx", 'i=pm&amp;folder=inbox'),
@@ -4702,6 +4704,9 @@ function page_header($page_title = '', $display_online_list = false, $item_id = 
 			$http_headers['X-PHPBB-IS-BOT'] = 'yes';
 		}
 	}
+	
+	// Load visible shops // mgomez // 2018-11-23
+	$template->assign_block_vars_array('shops', get_full_shops());
 
 	/**
 	* Execute code and/or overwrite _common_ template variables after they have been assigned.
--- includes/functions_shop.php
+++ includes/functions_shop.php
@@ -13,7 +13,28 @@ function get_shops() {
 	
 	return $shops;
 }
+
+function get_full_shops() {
+	global $db;
+	$shops = array();
+	
+	$query = $db->sql_query('SELECT shop_id, nombre, fa_icon, descripcion 
+								FROM ' . SHOPS_TABLE . " WHERE visible = 1");
+	while(($row = mysqli_fetch_assoc($query))) {
+		$shops[] = array(
+			'ID'			=>	$row['shop_id'],
+			'NOMBRE'		=>	$row['nombre'],
+			'FA_ICON'		=>	$row['fa_icon'],
+			'DESCRIPCION'	=>	$row['descripcion'],
+			'URL'			=>	get_shop_url($row['nombre'], $row['shop_id']),
+		);
+	}
+	$db->sql_freeresult($query);
+	
+	return $shops;
+}
 	
 function get_shop_url($shop_name, $shop_id) {
+	global $phpbb_root_path;
 	return append_sid("{$phpbb_root_path}tienda/" . title_to_url($shop_name) . '-c' . $shop_id);
 }
