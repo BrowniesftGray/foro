0edf8372dd8149598da9182140cda379b70afb8c
Gray <graybohemia@gmail.com>
2018-12-06 19:03:22 +0100

    Mod de ficha done

--- ext/gray/ficha/controller/main.php
+++ ext/gray/ficha/controller/main.php
@@ -149,10 +149,91 @@ class main
 	
     function borrar_personaje($pj) {
 
-    global $db;
+        global $db;
 
-    $db->sql_query("DELETE FROM personajes WHERE user_id = '$pj'");
-    $db->sql_query("DELETE FROM tecnicas WHERE pj_id = '$pj'");
-    $db->sql_query("DELETE FROM moderaciones WHERE pj_moderado = '$pj'");
+        $db->sql_query("DELETE FROM personajes WHERE user_id = '$pj'");
+        $db->sql_query("DELETE FROM tecnicas WHERE pj_id = '$pj'");
+        $db->sql_query("DELETE FROM moderaciones WHERE pj_moderado = '$pj'");
+    }
+
+    
+    public function viewMod($user_id)
+    {   
+        $grupo = $this->user->data['group_id'];
+        if ($grupo == 5 || $grupo == 4) {
+            get_ficha($user_id,$return = false, $ver = false);
+            $this->template->assign_vars(array(
+                'U_ACTION'              => append_sid('/ficha/storeMod/' . $user_id),
+            ));
+            return $this->helper->render('ficha_edit.html');
+        }
+        else
+        {
+            trigger_error("No puedes acceder a esta sección.");
+        }
+    }
+
+    public function storeMod($user_id)
+    {   
+        $grupo = $this->user->data['group_id'];
+
+        if ($grupo == 5 || $grupo == 4) {
+            $atrs = array(
+                'FUERZA'            => (int) request_var('atrFuerza', 1),
+                'RESISTENCIA'       => (int) request_var('atrVit', 1),
+                'AGILIDAD'          => (int) request_var('artAg', 1),
+                'ESPIRITU'          => (int) request_var('atrCCK', 1),
+                'CONCENTRACION'     => (int) request_var('atrCon', 1),
+                'VOLUNTAD'          => (int) request_var('atrVol', 1),
+            );
+            //profile_fields_data -> Tabla donde se encuentra la experiencia
+            $fields = array_merge(array(
+                    'NOMBRE'                => utf8_normalize_nfc(request_var('name', '', true)),
+                    'PJ_ID'                 => utf8_normalize_nfc(request_var('pj_id', '', true)),
+                    'EDAD'                  => utf8_normalize_nfc(request_var('edad', '', true)),
+                    'RANGO'                 => utf8_normalize_nfc(request_var('rango', '', true)),
+                    'ARQUETIPO'         => utf8_normalize_nfc(request_var('arquetipo', '', true)),
+                    'PRINCIPAL'         => utf8_normalize_nfc(request_var('ramaPrincipal', '', true)),
+                    'RAMA1'                 => utf8_normalize_nfc(request_var('ramaSec1', '', true)),
+                    'RAMA2'                 => utf8_normalize_nfc(request_var('ramaSec2', '', true)),
+                    'RAMA3'                 => utf8_normalize_nfc(request_var('ramaSec3', '', true)),
+                    'RAMA4'                 => utf8_normalize_nfc(request_var('ramaSec4', '', true)),
+                    'RAMA5'                 => utf8_normalize_nfc(request_var('ramaSec5', '', true)),
+                    'FISICO'                => utf8_normalize_nfc(request_var('descFis', '', true)),
+                    'CARACTER'          => utf8_normalize_nfc(request_var('descPsic', '', true)),
+                    'HISTORIA'          => utf8_normalize_nfc(request_var('descHis', '', true)),
+                    'TEC_JUTSUS'        => utf8_normalize_nfc(request_var('tecnicas', '', true)),
+                    'RAZON'                 => utf8_normalize_nfc(request_var('razon', '', true)),
+                ), $atrs);
+
+            $fields['HISTORIA'] = addslashes($fields['HISTORIA']);
+            str_replace('<br />', "\n", $fields['HISTORIA']);
+            $fields['FISICO'] = addslashes($fields['FISICO']);
+            str_replace('<br />', "\n", $fields['FISICO']);
+            $fields['CARACTER'] = addslashes($fields['CARACTER']);
+            str_replace('<br />', "\n", $fields['CARACTER']);
+            $idUsuario = $this->user->data['user_id'];
+
+            $sql = "UPDATE personajes SET ";
+            $sql .= "nombre = '{$fields['NOMBRE']}', edad = '{$fields['EDAD']}', rango = '{$fields['RANGO']}',";
+            $sql .= "clan = '{$fields['PRINCIPAL']}', rama1 = '{$fields['RAMA1']}', rama2 = '{$fields['RAMA2']}', rama3 = '{$fields['RAMA3']}', rama4 = '{$fields['RAMA4']}', rama5 = '{$fields['RAMA5']}',";
+            $sql .= 'tecnicas = "'.$fields['TEC_JUTSUS'].'",';
+            $sql .= "fuerza = '{$fields['FUERZA']}', vitalidad = '{$fields['RESISTENCIA']}', agilidad = '{$fields['AGILIDAD']}', cck = '{$fields['ESPIRITU']}', concentracion = '{$fields['CONCENTRACION']}', voluntad = '{$fields['VOLUNTAD']}',";
+            $sql .= "fisico = '{$fields['FISICO']}', psicologico = '{$fields['CARACTER']}', historia = '{$fields['HISTORIA']}'";
+            $sql .= "WHERE user_id = $user_id";
+            $this->db->sql_query($sql);
+            
+            if ($fields['ARQUETIPO'] != '') {
+                $sql = "UPDATE personajes SET arquetipo_id = '{$fields['ARQUETIPO']}' WHERE pj_id = '{$fields['PJ_ID']}'";
+                $this->db->sql_query($sql);
+            }
+            registrar_moderacion($fields);
+
+            trigger_error("Personaje moderado correctamente.");
+        }
+        else{
+            trigger_error("No eres moderador o administrador.");
+        }
+
     }
 }
