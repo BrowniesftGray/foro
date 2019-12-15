32f778709227c888629f42ef5dc0a5c994e92bc4
Gray <graybohemia@gmail.com>
2019-12-15 20:15:50 +0100

    Cambios tecnicas

--- ext/gray/tecnicas/controller/main.php
+++ ext/gray/tecnicas/controller/main.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace gray\tecnicas\controller;
+
 require_once('/home/shinobil/public_html/includes/functions_user.php');
 
 class main
@@ -49,43 +50,74 @@ class main
     {
         $this->validate_access();
 
-		    return $this->helper->render('tecnicas/home.html', 'Administrador de SL');
+        return $this->helper->render('tecnicas/home.html', 'Administrador de SL');
     }
 
-    function createTecnicas()
+    public function createTecnicas()
     {
         $this->validate_access();
 
         $this->template->assign_vars(array(
-    			'RAMA_OPTIONS'	=> $this->get_rama_options(),
-    		));
-        
-		    return $this->helper->render('tecnicas/create.html', 'Administrador de SL');
+                'RAMA_OPTIONS'	=> $this->get_rama_options(),
+            ));
+
+        return $this->helper->render('tecnicas/create.html', 'Administrador de SL');
     }
 
-    function store()
+    public function store()
     {
         $this->validate_access();
 
         $tecnica = array(
-            'ETIQUETA'       => (int) request_var('etiqueta', 1),
-            'NOMBRE'            => (int) request_var('name', 1),
-            'TIPO'          => (int) request_var('tipo', 1),
-            'RANGO'          => (int) request_var('rango', 1),
-            'SELLOS'     => (int) request_var('sellos', 1),
-            'REQUISITOS'          => (int) request_var('requisitos', 1),
-            'EFECTOS'          => (int) request_var('efectos', 1),
-            'DAMAGE_PV'          => (int) request_var('pv_damage', 1),
-            'DAMAGE_PC'          => (int) request_var('pc_damage', 1),
-            'DAMAGE_STA'          => (int) request_var('sta_damage', 1),
-            'DAMAGE_TURNO'          => (int) request_var('por_turno_damage', 1),
+            'ETIQUETA'          => utf8_normalize_nfc(request_var('etiqueta', '', true)),
+            'NOMBRE'            => utf8_normalize_nfc(request_var('name', '', true)),
+            'TIPO'              => utf8_normalize_nfc(request_var('tipo', '', true)),
+            'RANGO'             => utf8_normalize_nfc(request_var('rango', '', true)),
+            'SELLOS'            => utf8_normalize_nfc(request_var('sellos', '', true)),
+            'REQUISITOS'        => utf8_normalize_nfc(request_var('requisitos', '', true)),
+            'EFECTOS'           => utf8_normalize_nfc(request_var('efectos', '', true)),
+            'DAMAGE_PV'         => (int) request_var('pv_damage', 1),
+            'DAMAGE_PC'         => (int) request_var('pc_damage', 1),
+            'DAMAGE_STA'        => (int) request_var('sta_damage', 1),
+            'DAMAGE_TURNO'      => (int) request_var('por_turno_damage', 1),
             'COSTE_PV'          => (int) request_var('pv', 1),
             'COSTE_PC'          => (int) request_var('pc', 1),
-            'COSTE_STA'          => (int) request_var('sta', 1),
-            'COSTE_TURNO'          => (int) request_var('por_turno', 1),
-            'DESCRIPCION'          => (int) request_var('descripcion', 1),
+            'COSTE_STA'         => (int) request_var('sta', 1),
+            'COSTE_TURNO'       => (int) request_var('por_turno', 1),
+            'DESCRIPCION'       => utf8_normalize_nfc(request_var('descripcion', '', true)),
         );
 
+        // Coste en Puntos de Aprendizaje
+        switch ($tecnica['RANGO']) {
+          case 'E':
+            $coste_tecnica = 0;
+          break;
+          case 'D':
+            $coste_tecnica = 1;
+          break;
+          case 'C':
+            $coste_tecnica = 2;
+          break;
+          case 'B':
+            $coste_tecnica = 3;
+          break;
+          case 'A':
+            $coste_tecnica = 5;
+          break;
+          case 'S':
+            $coste_tecnica = 10;
+          break;
+        }
+
+        $rama = array(
+            'nombre'    => utf8_normalize_nfc(request_var('name', '', true)),
+            'RANGO'     => utf8_normalize_nfc(request_var('rango', '', true)),
+            'ETIQUETA'  => utf8_normalize_nfc(request_var('etiqueta', '', true)),
+            'coste'     => $coste_tecnica,
+            'rama_id'   => utf8_normalize_nfc(request_var('rama', '', true)),
+        );
+
+        $rama = utf8_normalize_nfc(request_var('rama', '', true));
         $etiqueta = $tecnica['ETIQUETA'];
         $etiqueta = "[".$etiqueta."][/".$etiqueta."]";
 
@@ -98,85 +130,93 @@ class main
         $damage = "";
         $contador = 0;
         if ($tecnica['DAMAGE_PV'] != "") {
-          $damage .= $tecnica['DAMAGE_PV'];
-          $contador++;
-        }
-        if ($tecnica['DAMAGE_STA'] != "" ) {
-          if ($contador != 0) {
-            $damage .= $damage.", ".$tecnica['DAMAGE_STA'];
+            $damage .= $tecnica['DAMAGE_PV'];
             $contador++;
-          }
-          else{
-            $damage .= $tecnica['DAMAGE_STA'];
+        }
+        if ($tecnica['DAMAGE_STA'] != "") {
+            if ($contador != 0) {
+                $damage .= $damage.", ".$tecnica['DAMAGE_STA'];
+                $contador++;
+            } else {
+                $damage .= $tecnica['DAMAGE_STA'];
+                $contador++;
+            }
+        }
+        if ($tecnica['DAMAGE_PC'] != "") {
+            if ($contador != 0) {
+                $damage .= $damage.", ".$tecnica['DAMAGE_PC'];
+                $contador++;
+            } else {
+                $damage .= $tecnica['DAMAGE_PC'];
+                $contador++;
+            }
+        }
+        if (isset($tecnica['DAMAGE_TURNO'])) {
+            $tecnica['DAMAGE_TURNO'] = true;
+            $damage .= $damage." por turno.";
+        } else {
+            $tecnica['DAMAGE_TURNO'] = false;
+        }
+
+
+        //Coste
+        $coste = "";
+        $contador = 0;
+
+        if ($tecnica['COSTE_PC '] != "") {
+            $coste .= $tecnica['COSTE_PC'];
             $contador++;
-          }
         }
-        if ($tecnica['DAMAGE_PC'] != "" ) {
-          if ($contador != 0) {
-            $damage .= $damage.", ".$tecnica['DAMAGE_PC'];
-            $contador++;
-          }
-          else{
-            $damage .= $tecnica['DAMAGE_PC'];
-            $contador++;
-          }
-        }
-      if (isset($tecnica['DAMAGE_TURNO'])) {
-        $damage .= $damage." por turno.";
-      }
 
-
-      //Coste
-      $coste = "";
-      $contador = 0;
-      if ($tecnica['COSTE_PC '] != "") {
-        $coste .= $tecnica['COSTE_PC'];
-        $contador++;
-      }
-      if ($tecnica['COSTE_STA'] != "" ) {
-        if ($contador != 0) {
-          $coste .= $coste.", ".$tecnica['COSTE_STA'];
-          $contador++;
+        if ($tecnica['COSTE_STA'] != "") {
+            if ($contador != 0) {
+                $coste .= $coste.", ".$tecnica['COSTE_STA'];
+                $contador++;
+            } else {
+                $coste .= $tecnica['DAMAGE_STA'];
+                $contador++;
+            }
         }
-        else{
-          $coste .= $tecnica['DAMAGE_STA'];
-          $contador++;
-        }
-      }
-      if ($tecnica['COSTE_PV'] != "" ) {
-        if ($contador != 0) {
-          $coste .= $coste.", ".$tecnica['COSTE_PV'];
-          $contador++;
-        }
-        else{
-          $coste .= $tecnica['COSTE_PV'];
-          $contador++;
-        }
-      }
-      if (isset($tecnica['COSTE_TURNO'])) {
-        $coste .= $coste." por turno.";
-      }
 
-      //Requisitos
-      $requisitos = explode("\n", $tecnica['REQUISITOS']);
-      $requisitos_texto = "<ul>";
-      foreach ($requisitos as $requisito) {
-        $requisitos_texto .= "<li>".$requisito."</li>";
-      }
-      $requisitos_texto .= "</ul>";
+        if ($tecnica['COSTE_PV'] != "") {
+            if ($contador != 0) {
+                $coste .= $coste.", ".$tecnica['COSTE_PV'];
+                $contador++;
+            } else {
+                $coste .= $tecnica['COSTE_PV'];
+                $contador++;
+            }
+        }
 
-      //Efectos
-      $efectos = $tecnica['EFECTOS'];
-      $efectos = str_replace("</ul>", "/#", $efectos);
-      $efectos = str_replace("</li>", "/$", $efectos);
-      $efectos = str_replace("<ul>", "#", $efectos);
-      $efectos = str_replace("<li>", "$", $efectos);
+        if (isset($tecnica['COSTE_TURNO'])) {
+            $tecnica['COSTE_TURNO'] = true;
+            $coste .= $coste." por turno.";
+        } else {
+            $tecnica['COSTE_TURNO'] = false;
+        }
 
-      //Tipos
-      $tipos = $tecnica['TIPOS'];
-      $tipos_texto = "";
-      foreach ($tipos as $tipo) {
-        switch ($tipo) {
+        //Requisitos
+        $requisitos = explode("\n", $tecnica['REQUISITOS']);
+        $requisitos_texto = "<ul>";
+
+        foreach ($requisitos as $requisito) {
+            $requisitos_texto .= "<li>".$requisito."</li>";
+        }
+        $requisitos_texto .= "</ul>";
+
+        //Efectos
+        $efectos = $tecnica['EFECTOS'];
+        $efectos = str_replace("</ul>", "/#", $efectos);
+        $efectos = str_replace("</li>", "/$", $efectos);
+        $efectos = str_replace("<ul>", "#", $efectos);
+        $efectos = str_replace("<li>", "$", $efectos);
+
+        //Tipos
+        $tipos = $tecnica['TIPOS'];
+        $tipos_texto = "";
+
+        foreach ($tipos as $tipo) {
+            switch ($tipo) {
           case 1:
             $tipos_texto .= "<tipo nin>Ninjutsu</tipo>";
             break;
@@ -199,30 +239,20 @@ class main
             $tipos_texto .= "<rango>Kinjutsu</rango>";
             break;
         }
-      }
+        }
 
-      $texto_tecnica = "<jutsu>".$nombre.$tipos_texto.$rango."<datos><b>Requisitos:</b>".$requisitos_texto."<b>Sellos:</b>".$sellos."</br><b>Efectos:</b>".$efectos;
-      if ($damage == "") {
-        $texto_tecnica .= "<b>Daño:</b>".$texto_tecnica.$damage."<br/>";
-      }
-      $texto_tecnica .= $texto_tecnica."<b>Coste:</b>".$coste."</datos><desc>".$descripcion."</desc><hr/><c><b onclick='selectCode(this)'>Código:</b><code>".$etiqueta."</code></c></jutsu>";
+        $texto_tecnica = "<jutsu>".$nombre.$tipos_texto.$rango."<datos><b>Requisitos:</b>".$requisitos_texto."<b>Sellos:</b>".$sellos."</br><b>Efectos:</b>".$efectos;
+        if ($damage == "") {
+            $texto_tecnica .= "<b>Daño:</b>".$texto_tecnica.$damage."<br/>";
+        }
+        $texto_tecnica .= $texto_tecnica."<b>Coste:</b>".$coste."</datos><desc>".$descripcion."</desc><hr/><c><b onclick='selectCode(this)'>Código:</b><code>".$etiqueta."</code></c></jutsu>";
 
-      crearBbcode($texto_tecnica, $etiqueta, $tecnica);
+        crearBbcode($texto_tecnica, $etiqueta, $tecnica, $rama);
     }
 
-    function crearBbcode($codigo, $etiqueta, $tecnica)
+    public function crearBbcode($codigo, $etiqueta, $tecnica, $rama)
     {
-      /*
-        Campos de la tabla
-          'bbcode_tag': $etiqueta
-          'bbcode_match': [$etiqueta][/$etiqueta]
-          'bbcode_tpl': $codigo
-          'first_pass_match': !\[$etiqueta\]\[/$etiqueta\]!i
-          'first_pass_replace': [$etiqueta:$uid][/$etiqueta:$uid]
-          'second_pass_replace': [$etiqueta:$uid][/$etiqueta:$uid]
-      */
-
-      $sql_array = array(
+        $sql_array = array(
         'bbcode_tag'=> $etiqueta,
         'bbcode_match'=> "[".$etiqueta."][/".$etiqueta."]",
         'bbcode_tpl'=> $codigo,
@@ -231,38 +261,42 @@ class main
         'second_pass_replace'=> "[".$etiqueta.":$uid][/".$etiqueta.":$uid]"
       );
 
-      $sql = "INSERT INTO ". BBCODE_TECNICAS . $this->db->sql_build_array('INSERT', $sql_array);
-      $this->db->sql_query($sql);
+        $sql = "INSERT INTO ". BBCODE_TECNICAS . $this->db->sql_build_array('INSERT', $sql_array);
+        $this->db->sql_query($sql);
 
-      $sql = "INSERT INTO ". TECNICAS_INFO . $this->db->sql_build_array('INSERT', $tecnica);
-      $this->db->sql_query($sql);
+        $sql = "INSERT INTO ". TECNICAS_INFO . $this->db->sql_build_array('INSERT', $tecnica);
+        $this->db->sql_query($sql);
 
+        if ($rama['rama_id'] != 0) {
+          $sql = "INSERT INTO ". TECNICAS_TABLE . $this->db->sql_build_array('INSERT', $rama);
+          $this->db->sql_query($sql);
+        }
     }
 
-    function get_rama_options()
+    public function get_rama_options()
     {
-      $options = "";
+        $options = "";
 
-      $query = $this->db->sql_query("SELECT rama_id, nombre FROM " . RAMAS_TABLE);
+        $query = $this->db->sql_query("SELECT rama_id, nombre FROM " . RAMAS_TABLE);
 
-      while ($row = $this->db->sql_fetchrow($query)){
-        $options .= "<option value='" . $row['rama_id'] . "'>" . $row['nombre'] . "</option>";
-      }
-      $this->db->sql_freeresult($query);
+        while ($row = $this->db->sql_fetchrow($query)) {
+            $options .= "<option value='" . $row['rama_id'] . "'>" . $row['nombre'] . "</option>";
+        }
+        $this->db->sql_freeresult($query);
 
-      return $options;
+        return $options;
     }
 
-  	function validate_access()
-  	{
-  		$grupo = $this->user->data['group_id'];
-          if ($grupo != 5) {
-              trigger_error("No puedes acceder a esta sección.");
-          }
-  	}
+    public function validate_access()
+    {
+        $grupo = $this->user->data['group_id'];
+        if ($grupo != 5) {
+            trigger_error("No puedes acceder a esta sección.");
+        }
+    }
 
-  	function get_return_link($view)
-  	{
-  		return "<br /><a href='/tecnicas/$view'>Volver</a>.";
-  	}
+    public function get_return_link($view)
+    {
+        return "<br /><a href='/tecnicas/$view'>Volver</a>.";
+    }
 }
